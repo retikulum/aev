e95f59f1e6e8909a496146f69b75302b9d666ee3
retikulum <burakcarikci1@gmail.com>
2022-11-19 17:50:46 +0300

    add clap & file,folder parameters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use evtx::EvtxParser;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args{
+    #[arg(long, default_value_t = String::from("C:/Windows/System32/winevt/Logs/Security.evtx") )]
+    file: String,
+    #[arg(long)]
+    folder: Option<String>,
+}
+
+
 fn main() {
-    println!("AEV!");
+    
+    let args = Args::parse();
+
+    let file = args.file;
+    let mut parser = EvtxParser::from_path(file).unwrap();
+    for record in parser.records() {
+        match record {
+            Ok(r) => println!("Record {}\n{}", r.event_record_id, r.data),
+            Err(e) => eprintln!("{}", e),
+        }
+    }
 }
